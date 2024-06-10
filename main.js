53de3e27a3e9eb96aecaea6a3e4327647ff50681
fgrSto <148966307+fgrSto@users.noreply.github.com>
2024-06-11 01:15:41 +0200

    Const & Variable

---
+++ main.js
@@ -0,0 +1,20 @@
+// Const of scores
+const _score = document.getElementById("score")
+const _bestScore = document.getElementById("b_score")
+const _streak = document.getElementById("streak")
+// Const of random numbers
+const _number1 = document.getElementById("number1")
+const _number2 = document.getElementById("number2")
+const _number3 = document.getElementById("number3")
+// Const of answer
+const _playerAnswer = document.getElementById("answer")
+const _button = document.getElementById("submit")
+
+// Variable of scores
+let score = 0
+let bestScore = 0
+let streak = 0
+// Variable of random numbers
+let number1 = 0
+let number2 = 0
+let number3 = 0
