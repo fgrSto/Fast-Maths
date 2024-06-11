85222e0ce7f0b4f405a1a038cd473af24b991fc9
fgrSto <148966307+fgrSto@users.noreply.github.com>
2024-06-11 17:35:29 +0200

    Fast css

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ const _number1 = document.getElementById("number1");
 const _number2 = document.getElementById("number2");
 const _number3 = document.getElementById("number3");
 
+const _sign1 = document.getElementById("sign1");
+const _sign2 = document.getElementById("sign2");
+
 const _playerAnswer = document.getElementById("answer");
 const _button = document.getElementById("submit");
 
@@ -15,13 +18,13 @@ const _button = document.getElementById("submit");
 
 //#region Let
 
-let score = 0;
-let bestScore = 0;
-let streak = 0;
+let score = null;
+let bestScore = null;
+let streak = null;
 
-let number1 = 0;
-let number2 = 0;
-let number3 = 0;
+let number1 = null;
+let number2 = null;
+let number3 = null;
 
 let answer = null;
 let playerAnswer = null;
@@ -54,11 +57,12 @@ function Calculs() {
   number1 = Math.floor(Math.random() * 100);
   number2 = Math.floor(Math.random() * 100);
   answer = number1 + number2;
-  
-  _number1.innerHTML = number1
-  _number2.innerHTML = number2
+
+  _number1.innerHTML = number1;
+  _number2.innerHTML = number2;
 }
 
+// Check if the answer is right
 function CheckAnswer() {
   playerAnswer = _playerAnswer.value;
   if (answer == playerAnswer) {
@@ -69,6 +73,7 @@ function CheckAnswer() {
   }
 }
 
+// Manage all scoring systeme
 function Scoring() {
   if (result == true) {
     score = score + 1;
@@ -89,12 +94,22 @@ function Scoring() {
   Calculs();
 }
 
+function Estatic() {
+  if (number3 == 0) {
+    _number3.classList.add("hidden");
+    _sign2.classList.add("hidden");
+  }
+}
+
 // Executing functions
 Init();
 Calculs();
+Estatic();
 
+// Execute function when player give answer
 _button.addEventListener("click", (e) => {
   CheckAnswer();
   Scoring();
-  _playerAnswer.value = null
+  Estatic();
+  _playerAnswer.value = null;
 });
