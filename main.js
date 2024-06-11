73afd782d10263bd29303b0b8a7ab364b02f5a38
fgrSto <148966307+fgrSto@users.noreply.github.com>
2024-06-11 17:19:35 +0200

    Playable

--- main.js
+++ main.js
@@ -1,50 +1,100 @@
-// Const of scores
+//#region Const
+
 const _score = document.getElementById("score");
 const _bestScore = document.getElementById("b_score");
 const _streak = document.getElementById("streak");
-// Const of random numbers
+
 const _number1 = document.getElementById("number1");
 const _number2 = document.getElementById("number2");
 const _number3 = document.getElementById("number3");
-// Const of answer
+
 const _playerAnswer = document.getElementById("answer");
 const _button = document.getElementById("submit");
 
-// Variable of scores
+//#endregion
+
+//#region Let
+
 let score = 0;
 let bestScore = 0;
 let streak = 0;
-// Variable of random numbers
+
 let number1 = 0;
 let number2 = 0;
 let number3 = 0;
+
 let answer = null;
 let playerAnswer = null;
+
+let result = false;
 let multiplicator = 1;
 
-function VerifyAnswer() {
+//#endregion
+
+// Reset all value
+function Init() {
+  score = 0;
+  bestScore = 0;
+  streak = 0;
+  number1 = 0;
+  number2 = 0;
+  number3 = 0;
+  answer = null;
+  playerAnswer = null;
+  result = false;
+  multiplicator = 1;
+
+  _score.innerHTML = "Score : " + score;
+  _bestScore.innerHTML = "Best Score : " + bestScore;
+  _streak.innerHTML = "Streak : " + streak;
+}
+
+// Calculate
+function Calculs() {
+  number1 = Math.floor(Math.random() * 100);
+  number2 = Math.floor(Math.random() * 100);
+  answer = number1 + number2;
+  
+  _number1.innerHTML = number1
+  _number2.innerHTML = number2
+}
+
+function CheckAnswer() {
   playerAnswer = _playerAnswer.value;
   if (answer == playerAnswer) {
-    console.log("w");
-    score = score * multiplicator + 1;
-    _score.innerHTML = "Score : " + score;
-    console.log(score);
-  } else {
-    GameOver();
-    BestScore();
-    score = 0;
-    multiplicator = 1;
+    result = true;
+  }
+  if (answer != playerAnswer) {
+    result = false;
   }
 }
 
-answer = number1 + number2;
-console.log("answer = " + answer);
+function Scoring() {
+  if (result == true) {
+    score = score + 1;
+    streak += 1;
+  }
+  if (result == false) {
+    if (score > bestScore) {
+      bestScore = score;
+    }
+    score = 0;
+    streak = 0;
+  }
 
-_score.innerHTML = "Score : " + score;
-_number1.innerHTML = number1;
-_number2.innerHTML = number2;
-_number3.innerHTML = number3;
+  _score.innerHTML = "Score : " + score;
+  _bestScore.innerHTML = "Best Score : " + bestScore;
+  _streak.innerHTML = "Streak : " + streak;
+
+  Calculs();
+}
+
+// Executing functions
+Init();
+Calculs();
 
 _button.addEventListener("click", (e) => {
-  VerifyAnswer();
+  CheckAnswer();
+  Scoring();
+  _playerAnswer.value = null
 });
